eab2dd8a07bac9f9e4677ea31ac32a0e817fa271
BobRazoswki <bobongithub@gmail.com>
2014-06-30 17:17:07 +0100

    middle day

--- lib/student-directory.rb
+++ lib/student-directory.rb
@@ -2,13 +2,8 @@ def print_header
 	puts 'Welcome to Student Directory of Makers Academy'
 end
 
-def print_
-	input = $STDIN
-   puts '#{STDIN}'
-end
-
-def menu
-	puts "------ ~~ MAIN MENU ~~ ------"
+def print_menu
+    puts "------ ~~ MAIN MENU ~~ ------"
     puts "1. Add students"
     puts "2. Show all the students"
     puts "3. Show all the students that start with an 'a'"
@@ -19,4 +14,42 @@ def menu
     puts "8. Try Load the students"
     puts "9. EXIT the awesome program"
     puts "10. Show the source code"
+    puts "Type here your number"
+end
+
+def students
+    @students = [{name: name, cohort: cohort}]
+end
+
+def get_user_input(input = $stdin)
+    input.gets
+end
+
+=begin
+def case_selection
+case @selection
+when "1"
+    get_user_input
+when "2"
+    display_student
+when "9" 
+    exit
+
+end
+=end
+
+
+def display_student(student)
+    # input = $STDIN
+    puts format_student(student)
+end
+
+def format_student(student)
+    "#{student[:name]} #{student[:cohort]}"
+end
+
+
+
+def print_footer
+    puts "Now we have #{students.length} students"
 end
--- spec/student_directory_spec.rb
+++ spec/student_directory_spec.rb
@@ -10,16 +10,65 @@ describe 'Student Directory' do
 
 		it 'the menu is printed' do
 			expect(self).to receive(:print_menu)
-			menu
+			print_menu
+		end
+=begin
+		it 'user is prompted for action' do
+			input = "1\n"
+			allow($stdin).to receive(:gets).and_return(input)
+			expect(get_user_input()).to eq '1'	
+		end
+=end
+	end 
+
+	context 'when the program is working' do
+		context 'the questionnaire collects each students information' do
+
+
+			it 'adds students names' do
+				student = {name: "Stephen", cohort: "June"}
+				expect(self).to receive(:puts).with("Stephen June")
+				display_student(student)
+			end
+
+			it "should format student correctly" do
+				student = {name: "Stephen", cohort: "June"}
+				expect(format_student(student)).to eq "Stephen June"
+			end
+
+			it 'adds students cohorts' do
+			end
 		end
 
-		it 'user is prompted for input' do
-			input = STDIN
-			expect(self).to receive(:puts).with('#{STDIN}')
-			print_
+		context 'it shows a list of all students information' do
+			it 'prints a list of all student information' do
+				expect puts #{@students}
+			end
 		end
 
-	
+		context "when the user enters" do
+
+			it "1" do
+				input = $stdin
+				allow($stdin).to receive(:gets).and_return(input)
+				expect(get_user_input($stdin)).to eq $stdin
+			end
+
+			it "2" do
+				end
+
+		end
+
+
+	end
+
+
+	context 'when the program finishes' do
+
+		it 'a summary of our students is printed' do
+			expect(self).to receive(:print_footer)
+			print_footer
+		end
 	end
 
 end
